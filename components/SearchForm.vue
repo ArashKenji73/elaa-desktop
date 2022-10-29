10dcfad9145868ce4cee62ddf3e5e32217c5da26
kenjiarash <kenjiarash@gmail.com>
2022-10-29 14:36:13 +0330

    fix : search form component

---
+++ components/SearchForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="search-box">
+        <span class="icon-search-normal search-icon text-xs"></span>
+        <input type="text" class="search-input iranSansNumb" placeholder=".... جستجوی محصولات" />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.search-box{
+    display: flex;
+    background: #F8F8F8;
+    border: 1px solid #E3E3E3;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    width: 242px;
+    input{
+        @apply flex-grow text-xs;
+        background: none;
+        &:focus{
+            outline: none;
+        }
+    }
+    .search-icon{
+        @apply flex items-center justify-start w-8 ml-0;
+    }
+}
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -29,14 +29,7 @@
             <!-- LOGO-SEARCHFORM -->
             <div class="logo-searchForm">
                 <!-- SEARCHFORM -->
-                <div class="search-box">
-                    <span class="icon-search-normal search-icon text-xs"></span>
-                    <input
-                        type="text"
-                        class="search-input iranSansNumb"
-                        placeholder=".... جستجوی محصولات"
-                    />
-                </div>
+                <search-form/>
                 <!-- LOGO -->
                 <img class="logo" src="~/assets/images/logo.svg" />
             </div>
@@ -79,24 +72,5 @@ header {
 
 .logo{
     width: 50px;
-}
-
-.search-box{
-    display: flex;
-    background: #F8F8F8;
-    border: 1px solid #E3E3E3;
-    padding: 0.5rem 1rem;
-    border-radius: 0.5rem;
-    width: 242px;
-    input{
-        @apply flex-grow text-xs;
-        background: none;
-        &:focus{
-            outline: none;
-        }
-    }
-    .search-icon{
-        @apply flex items-center justify-start w-8 ml-0;
-    }
 }
 </style>
