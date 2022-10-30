f1954b0926ba1e03f2c8d7d99181592d792eb0eb
kenjiarash <kenjiarash@gmail.com>
2022-10-30 08:42:59 +0330

    fix : tailwind container

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,21 @@ module.exports = {
     container: {
       // you can configure the container to be centered
       center: true,
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1320px',
+      },
     },
     extend: {
       colors:{
         'elaa-violet': '#A993DB',
-        'elaa-light-violet': '#F3F3FF'
+        'elaa-light-violet': '#F3F3FF',
+        'elaa-mint': '#BAE0E3',
+        'elaa-mint-dark': '#608E92',
+        'elaa-khaki': '#DED1CA'
       }
     },
   },
