d169b1bb2da2a9f0610dd789be03f19db05e1f64
kenjiarash <kenjiarash@gmail.com>
2022-11-01 10:09:06 +0330

    add : categories slider

---
+++ components/TheCategorySlider.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+        <div class="category-pattern"></div>
+        <div class="slider container">
+            <h1 class="title">دسته بندی ها</h1>
+
+            <div class="scrolling-wrapper">
+                <div id="scroll">
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    // const scrollContainer = document.getElementById("scroll");
+
+    // scrollContainer.addEventListener("wheel", (evt) => {
+    //     evt.preventDefault();
+    //     scrollContainer.scrollLeft += evt.deltaY;
+    // });
+
+
+    const slider = document.getElementById('scroll');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+        //slider.style.transform = "scale(1.015)";
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+        //slider.style.transform = "scale(1)";
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 1; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+    });
+})
+</script>
+
+<style lang="scss">
+.category-pattern {
+    @apply w-full h-56;
+    background-image: url('~/assets/images/saw-pattern.svg');
+}
+
+.slider {
+    @apply flex flex-col gap-4 -mt-36;
+
+    .title {
+        font-family: IRANYekanExtraBoldFaNum;
+        @apply text-xl;
+    }
+
+    .scrolling-wrapper {
+        @apply p-4 bg-white rounded-lg border-2 border-elaa-light-violet;
+        #scroll{
+            @apply flex gap-4 overflow-x-hidden overflow-y-hidden transition-all duration-300;
+        }
+        .card {
+            min-width: 136px;
+            min-height: 180px;
+            background: #ddd;
+            @apply rounded-md;
+        }
+    }
+}
+</style>
