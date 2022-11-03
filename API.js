716a94b372f2ebc8b2899ebcc8135d4c76ab2b8f
kenjiarash <kenjiarash@gmail.com>
2022-11-03 16:53:41 +0330

    add : top sales to api

--- API.js
+++ API.js
@@ -1,1 +1,4 @@
 export const getCategories = 'api/Categories/index.json'
+export function getTopSales(limit=20, page=1){
+    return `api/Picture3ds/filterPicture3ds.json?limit=${limit}&page=${page}&order_by=sales&order_mode=desc`
+}
