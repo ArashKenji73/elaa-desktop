e77de80d62c41037d26ab45bbe7618f6a6fa216a
kenjiarash <kenjiarash@gmail.com>
2022-11-04 10:35:32 +0330

    add : error , loading state

--- components/TheCategorySlider.vue
+++ components/TheCategorySlider.vue
@@ -3,10 +3,20 @@
         <div class="category-pattern"></div>
         <div class="slider container">
             <h1 class="title">دسته بندی ها</h1>
-            <div v-if="pending">
-                Loading ...
+            <template v-if="pending">
+                <div class="scrolling-wrapper">
+                    <div class="loading">
+                        <template v-for="(category,i) in 12">
+                            <category-card-loading />
+                        </template>
+                    </div>
+                </div>
+            </template>
+            <div v-if="!pending && error">
+                error !
+                <button @click.prevent="load">Load post</button>
             </div>
-            <div class="scrolling-wrapper">
+            <div class="scrolling-wrapper" v-if="!pending && !error">
                 <div id="scroll">
                     <template v-for="(category,i) in data.categories">
                         <category-card :category="category"/>
@@ -21,9 +31,13 @@
 import { onMounted } from 'vue'
 import {getCategories} from '@/API.js'
 const runtimeConfig = useRuntimeConfig()
-const { data, pending, error } = await useLazyFetch(getCategories,{
+const { data, pending, error, refresh } = await useLazyFetch(getCategories,{
     baseURL: runtimeConfig.public.apiBase,
 })
+
+const load = () => {
+  refresh()
+}
 
 onMounted(() => {
     // const scrollContainer = document.getElementById("scroll");
@@ -81,9 +95,11 @@ onMounted(() => {
 
     .scrolling-wrapper {
         @apply p-4 bg-white rounded-lg border-2 border-elaa-light-violet;
-        #scroll{
+        #scroll,.loading{
             @apply flex gap-4 overflow-x-hidden overflow-y-hidden transition-all duration-300;
         }
     }
 }
+
+
 </style>
