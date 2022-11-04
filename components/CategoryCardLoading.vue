1fed38623a03257215e32538691e895cde74bd01
kenjiarash <kenjiarash@gmail.com>
2022-11-04 10:34:44 +0330

    add : add loading category card

---
+++ components/CategoryCardLoading.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="category-card-loading"></div>
+</template>
+
+<style lang="scss" scoped>
+.category-card-loading {
+    min-width: 136px;
+    min-height: 180px;
+    @apply  animate-pulse bg-gray-200 py-5 px-2 rounded-md cursor-pointer;
+}
+</style>
