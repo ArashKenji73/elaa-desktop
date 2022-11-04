1c9181c7d7e8bf205f01187fcb1651a8fe624a26
kenjiarash <kenjiarash@gmail.com>
2022-11-04 10:34:09 +0330

    fix : categories shorten name

--- components/CategoryCard.vue
+++ components/CategoryCard.vue
@@ -6,9 +6,28 @@
 </template>
 
 <script setup>
+//import { computed } from 'vue'
 const props = defineProps(['category'])
 const runtimeConfig = useRuntimeConfig()
 const baseURL = runtimeConfig.public.apiBase;
+const limitChar = 34
+
+// const shortenName = computed(() => {
+//     if (props.category.name) {
+//         if (props.category.name.length > limitChar) {
+//           return props.category.name.substr(0, limitChar) + " ...";
+//         } else {
+//           return props.category.name;
+//         }
+//       }else{
+//         if (props.category.name > limitChar) {
+//           return props.category.name.substr(0, limitChar) + " ...";
+//         } else {
+//           return props.category.name;
+//         }
+//       }
+//     }
+// )
 </script>
 
 <style lang="scss">
