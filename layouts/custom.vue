703baa0461baa2333a9c5d991232d1f02550fddd
kenjiarash <kenjiarash@gmail.com>
2022-12-16 16:50:27 +0330

    fix : main layout min height

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,8 +1,18 @@
 <template>
-    <div>
+    <div class="main-layout">
         <TheHeader />
         <TheNavbar />
-        <slot />
+        <div class="main-content">
+            <slot />
+        </div>
         <TheFooter />
     </div>
 </template>
+<style lang="scss">
+.main-layout{
+    @apply flex flex-col min-h-screen
+}
+.main-content{
+    @apply flex-grow;
+}
+</style>
